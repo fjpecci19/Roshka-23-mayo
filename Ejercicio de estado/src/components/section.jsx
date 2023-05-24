f7ccab74b69f9d2df9c233ea4c12b1e3a3e3ffa0
Franco Pecci <fapeccisan@gmail.com>
2023-05-24 08:45:51 -0400

    l

--- Ejercicio de estado/src/components/section.jsx
+++ Ejercicio de estado/src/components/section.jsx
@@ -3,11 +3,18 @@ import "./section.css"
 import { useState } from 'react'
 
 function Section({element}){
+    const toggledespegable = () => {
+        var despegable = document.getElementById(".elemprincipal")
+        var contenido = document.getElementById("sub")
+
+        contenido.style.display = (contenido.style.display === "none") ? "block" : "none"
+
+    }
+    
     return (
         <div className="sideboard">
-            <div className="Section">
-                {element.elem}
-            </div>
+            <div className="Section" onClick={toggledespegable}>{element.elem}<div className="subSection" id="sub">{element.subelem}</div></div>
+            
         </div>
     )
 }
